d452db2ae26a1566dfe9fe04862c2a5adc682676
rudra <rudra8997@gmail.com>
2023-01-28 07:41:29 +0000

    superover

---
+++ index.js
@@ -0,0 +1,9 @@
+const element = (
+
+     <h1 className="imageContainer">Super Over League</h1>;
+     <div className="imageContainer"><img src="https://assets.ccbp.in/frontend/react-js/rcb-img.png" className="pictures"/><img src="https://assets.ccbp.in/frontend/react-js/csk-img.png" className="pictures"/></div>
+
+//   Write your code here.
+)
+
+ReactDOM.render(element, document.getElementById('root'))
